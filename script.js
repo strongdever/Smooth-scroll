6fcc21af6586c4431204c3a1209b9b6f6d831050
strongdever <kstronger77@gmail.com>
2023-08-20 00:25:55 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const body = document.body,
+      jsScroll = document.getElementsByClassName('htmlclass')[0],
+      height = jsScroll.getBoundingClientRect().height - 1,
+      speed = 0.05
+
+var offset = 0
+
+body.style.height = Math.floor(height) + "px"
+
+function smoothScroll() {
+    offset += (window.pageYOffset - offset) * speed
+    
+    var scroll = "translateY(-" + offset + "px) translateZ(0)"
+    jsScroll.style.transform = scroll
+    
+    raf = requestAnimationFrame(smoothScroll)
+}
+smoothScroll()
+
